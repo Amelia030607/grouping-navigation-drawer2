0ec7c40d7982dde4fc32bf1f5033e3fcc5d0343a
amelia030607 <riskiamelia.xpplg2@gmail.com>
2024-11-09 08:36:24 +0700

    Seccond_commit

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -19,32 +19,37 @@ class MyHomePage extends StatelessWidget {
         foregroundColor: Colors.white,
       ),
       drawer: buildGroupDrawer(context),
-      body: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-          shrinkWrap: true,
-          children: List.generate(10, (index) {
-            return Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                height: 5.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.blue[200],
-                ),
-                child: Text(
-                  "Materi $index",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 20.0,
-                  ),
-                ),
-              ),
-            );
-          })),
+      body: Center(
+        child: Text("pemrograman mobile $title"),
+      ),
     );
+    //body: GridView.count(
+    //crossAxisCount: 2,
+    //crossAxisSpacing: 10.0,
+    //mainAxisSpacing: 10.0,
+    //shrinkWrap: true,
+    //children: List.generate(10, (index) {
+    //return Padding(
+    //padding: const EdgeInsets.all(10.0),
+    //child: Container(
+    //height: 5.0,
+    //decoration: BoxDecoration(
+    //borderRadius: BorderRadius.circular(20.0),
+    //color: Colors.blue[200],
+    //),
+    //child: Text(
+    //"Materi $index",
+    //textAlign: TextAlign.center,
+    //textAlign: TextAlign.center,
+    //style: const TextStyle(
+    //color: Colors.black,
+    //fontSize: 20.0,
+    // ),
+    //),
+    // ),
+    // );
+    // })),
+    // );
   }
 
   Widget buildGroupDrawer(context) {
@@ -54,83 +59,107 @@ class MyHomePage extends StatelessWidget {
         children: [
           DrawerHeader(
             decoration: BoxDecoration(color: Colors.blue[900]),
-            child: const Text(
-              "Riski Amelia Wiji Astuti",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue[900]),
+              accountName: Text("Riski Amelia Wiji Astuti",
+              style : TextStyle(
+                fontSize : 18.0,
               ),
-            ),
+              ),
+              accountEmail: Text("riskiamelia.xpplg2@gmail.com",),
+              currentAccountPictureSize: Size.square(40.0),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("2",
+                style: TextStyle(fontSize: 25.0,
+                color: Colors.blue,
+                ),
+                ),
+                ),
+              ),
+            //child: const Text(
+              //"Riski Amelia Wiji Astuti",
+              //style: TextStyle(
+                //color: Colors.white,
+          //       fontSize: 30.0,
+          //     ),
+          //   ),
+          // ),
+          // ExpansionTile(
+          //   title: Text("Materi"),
+          //   children: [
+          //     ListTile(
+          //       title: Text("Materi 1"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text("Materi 2"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text("Materi 3"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text("Materi 4"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text("Materi 5"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //   ],
+          // ),
+          // ExpansionTile(
+          //   title: Text("Tugas"),
+          //   children: [
+          //     ListTile(
+          //       title: Text("Tugas 1"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text("Tugas 2"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text("Tugas 3"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text("Tugas 4"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+          //     ListTile(
+          //       title: Text("Tugas 5"),
+          //       onTap: () {
+          //         Navigator.pop(context);
+          //       },
+          //     ),
+           // ],
           ),
-          ExpansionTile(
-            title: Text("Materi"),
-            children: [
-              ListTile(
-                title: Text("Materi 1"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text("Materi 2"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text("Materi 3"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text("Materi 4"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text("Materi 5"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-          ExpansionTile(
-            title: Text("Tugas"),
-            children: [
-              ListTile(
-                title: Text("Tugas 1"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text("Tugas 2"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text("Tugas 3"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text("Tugas 4"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text("Tugas 5"),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
+          ListTile(
+            leading: Icon (Icons.person),
+            title: Text("Amel"),
+            subtitle: Text("XII RPL 2"),
+            trailing: Icon (Icons.chevron_right),
           ),
         ],
       ),
